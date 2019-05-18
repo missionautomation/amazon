f464ac2769134b6b2b2b2cc3f9c5dd14784fadc8
garimagupta-18 <50354811+garimagupta-18@users.noreply.github.com>
2019-05-18 09:20:44 +0530

    Revert "Global components"
    This reverts commit ec1e667bec75e5354d490d44bb29c0389ab50494.

--- TestAutomation/src/test/java/pageObjectModel/HomePageData.java
+++ TestAutomation/src/test/java/pageObjectModel/HomePageData.java
@@ -1,18 +1,5 @@
 package pageObjectModel;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 public class HomePageData {
-	
-	@FindBy(id="twotabsearchtextbox")
-	public WebElement searchTextBox;
-	
-	@FindBy(className="nav-input")
-	public WebElement searchIcon;
-	
-	@FindBy(id="searchDropdownBox")
-	public WebElement categorySearchDropDown;
-	
-	
+
 }
