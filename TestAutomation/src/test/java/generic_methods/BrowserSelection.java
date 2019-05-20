0e27e8c549ae60ac51d01b816c6e8f77b185224a
garimagupta-18 <50354811+garimagupta-18@users.noreply.github.com>
2019-05-20 16:47:47 +0530

    Updated Browser selection in generic methods

--- TestAutomation/src/test/java/generic_methods/BrowserSelection.java
+++ TestAutomation/src/test/java/generic_methods/BrowserSelection.java
@@ -2,22 +2,25 @@ package generic_methods;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class BrowserSelection {
-	static String browser = "chrome";
 	WebDriver driver;
-	public void getBrowser() {
+	public void getBrowser(String browser) {
 		if(browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 			driver = new ChromeDriver();
 		} else if(browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 			driver = new FirefoxDriver();
-		} else if(browser.equalsIgnoreCase("internet explorer")) {
+		} else if(browser.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
 			driver = new InternetExplorerDriver();
-		} else if(browser.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+		} else {
+			System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+			driver = new ChromeDriver();
 		}
+		driver.manage().window().maximize();
 	}
 }
